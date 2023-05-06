146a0bdcf7414005c1bc6a99da0af32f83ecbe40
PCBL <cksgodl1@naver.com>
2023-05-06 11:39:15 +0900

    design : 음성통화 뷰 작성

--- app/src/main/java/kau/sohothackathon/compileerror/MainActivity.kt
+++ app/src/main/java/kau/sohothackathon/compileerror/MainActivity.kt
@@ -1,43 +1,48 @@
 package kau.sohothackathon.compileerror
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kau.sohothackathon.compileerror.ui.theme.CompileerrorTheme
+import kau.sohothackathon.compileerror.ui.voice.VoiceCallScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        checkPermission()
+
         setContent {
             CompileerrorTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    VoiceCallScreen()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CompileerrorTheme {
-        Greeting("Android")
+    private fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                1004
+            )
+        }
     }
 }
---
+++ app/src/main/java/kau/sohothackathon/compileerror/ui/voice/VoiceCallScreen.kt
@@ -0,0 +1,80 @@
+package kau.sohothackathon.compileerror.ui.voice
+
+import android.content.Context
+import android.media.*
+import android.os.Environment
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import kau.sohothackathon.compileerror.R
+import kau.sohothackathon.compileerror.RecordingThread
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+
+
+@Composable
+fun VoiceCallScreen() {
+
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val ouputPlayer: MediaPlayer =
+        MediaPlayer.create(context, R.raw.levitating)
+
+    DisposableEffect(key1 = Unit) {
+        playVoiceCall(context, ouputPlayer)
+//        AudioCutter.cutAudio(context, R.raw.levitating)
+        onDispose {
+            ouputPlayer.release()
+        }
+    }
+    val outputDirectory =
+        File(context.getExternalFilesDir(Environment.DIRECTORY_MUSIC), "input")
+    outputDirectory.mkdirs()
+    val mp3InputStream = context.resources.openRawResource(R.raw.time_is_running_out)
+    val tempFile = File.createTempFile("prefix", ".mp3", context.cacheDir)
+    FileOutputStream(tempFile).use { outputStream ->
+        mp3InputStream.copyTo(outputStream)
+    }
+
+    val thread = RecordingThread(outputDirectory.absolutePath, tempFile)
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(text = "음성통화 화면")
+        Button(onClick = {
+            playVoiceCall(context, ouputPlayer)
+        }) {
+            Text(text = "통화종료")
+        }
+        Button(onClick = {
+            scope.launch {
+                thread.start()
+                delay(5000L)
+                thread.stopRecording()
+            }
+        }) {
+            Text(text = "녹음시작")
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "녹음종료")
+        }
+    }
+
+}
+
+
+fun playVoiceCall(context: Context, mediaPlayer: MediaPlayer) {
+    try {
+        mediaPlayer.start()
+        mediaPlayer.setOnCompletionListener { mediaPlayer -> mediaPlayer.release() }
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+}
