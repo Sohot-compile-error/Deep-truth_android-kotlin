6f5dfad6725c1d8f02d0aac4a3266b4bcfc827d8
dlgocks1 <cksgodl1@naver.com>
2023-06-05 14:16:09 +0900

    assets : BottomNav관련 에셋추가

--- app/src/main/java/kau/sohothackathon/compileerror/util/Constants.kt
+++ app/src/main/java/kau/sohothackathon/compileerror/util/Constants.kt
@@ -7,9 +7,13 @@ object Constants {
     const val ADDRESS_ROUTE = "nav-address"
     const val VOICE_CALL_ROUTE = "nav-voice-call"
     const val VIDEO_CALL_ROUTE = "nav-video-call"
+    const val FILE_ROUTE = "nav-file-route"
 
     // Graphs
     const val ADDRESS_GRAPH = "address_graph"
+    const val FILE_GRAPH = "file_graph"
     const val ONBOARD_GRAPH = "onboard_graph"
+    const val MAIN_GRAPH = "main_graph"
+
 
 }
---
+++ app/src/main/java/kau/sohothackathon/compileerror/util/Screen.kt
@@ -0,0 +1,24 @@
+package kau.sohothackathon.compileerror.util
+
+import android.support.annotation.DrawableRes
+import kau.sohothackathon.compileerror.R
+import kau.sohothackathon.compileerror.util.Constants.ADDRESS_ROUTE
+import kau.sohothackathon.compileerror.util.Constants.FILE_ROUTE
+
+enum class Screen(
+    val route: String,
+    @DrawableRes val selectedIcon: Int,
+    @DrawableRes val unselectedIcon: Int
+) {
+    ADDRESS(
+        ADDRESS_ROUTE,
+        R.drawable.ic_baseline_person_24,
+        R.drawable.ic_outline_person_24
+    ),
+    FILE(
+        FILE_ROUTE,
+        R.drawable.ic_baseline_insert_drive_file_24,
+        R.drawable.ic_outline_insert_drive_file_24
+    );
+
+}
